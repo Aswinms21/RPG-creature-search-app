ddeb045f987e78b4a9d6d2d099343a27cd8c2e86
Aswin M S <achums2121@gmail.com>
2025-11-05 21:03:47 +0530

    Delete js directory

--- js/script1.js
+++
@@ -1,7 +0,0 @@
-let email=document.getElementById("email");
-let password=document.getElementById("password");
-const submitBtn=document.getElementById("submit-btn");
-
-submitBtn.addEventListener("click", function(){
-    if(email)
-});
